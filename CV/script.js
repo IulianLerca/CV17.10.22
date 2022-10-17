b3938f7959b6bfc4aa4474e13a421e1045452d60
IulianLerca <116027424+IulianLerca@users.noreply.github.com>
2022-10-17 21:15:51 +0300

    Add files via upload

---
+++ CV/script.js
@@ -0,0 +1,12 @@
+function afiseazaText() {
+    document.querySelector("#experienta1").classList.toggle("element-ascuns")
+    
+
+    //document.querySelector -> ne returneaza elementul
+    //classList ne ofera o lista cu clasele
+    // togle va adauga o clasa in cazul in care ea nu este in lista, si o va sterge daca este in lista ( se aplica doar pt Classlist)
+
+}
+function afiseazaText2(){
+    document.querySelector("experienta2").classList.toggle("element-ascunt")
+}
